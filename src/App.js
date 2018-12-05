815810e07389723762a25a03b7f17b1f156c8a3b
Kylin93CN <375020612@qq.com>
2018-12-05 11:23:34 +0800

    videojs demo commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const App = () => <div>123</div>;
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [{
+    src: 'http://lvxiang.site/public/dy003.mp4',
+    type: 'video/mp4',
+  }],
+};
+
+const App = () => (
+  <VideoPlayer {...videoJsOptions} />
+);
 
 export default App;
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+
+export default class VideoPlayer extends Component {
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div data-vjs-player>
+          <video ref={node => { this.videoNode = node; }} className="video-js" />
+        </div>
+      </div>
+    );
+  }
+}
